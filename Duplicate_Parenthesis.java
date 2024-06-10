98ac30c736f56b29283c37f8f5d830ea1c57a7e6
Ayush2649 <142195140+Ayush2649@users.noreply.github.com>
2024-06-10 10:36:22 +0530

    Add files via upload

---
+++ Duplicate_Parenthesis.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Duplicate_Parenthesis{
+    public static boolean isDuplicate(String str){
+        Stack<Character> s = new Stack<>();
+
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+
+            if(ch == ')'){
+                int count = 0;
+                while(s.peek() != '('){
+                    s.pop();
+                    count++;
+                }
+                if(count < 1){
+                    return true;
+                } else {
+                    s.pop();
+                }
+            } else {
+                s.push(ch);
+            }
+        }
+        return false;
+    }
+
+    public static void main(String args[]){
+        String str = "((a+b))"; //True
+        String str2 = "(a+b)"; //False
+        System.out.println(isDuplicate(str2));
+    }
+}
