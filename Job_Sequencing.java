55346a2587ef253679a8c2474f137fa019f1db07
Ayush2649 <142195140+Ayush2649@users.noreply.github.com>
2024-06-15 08:10:56 +0530

    Add files via upload

---
+++ Job_Sequencing.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Job_Sequencing {
+
+    static class Job {
+        int deadline;
+        int profit;
+        int id;
+
+        public Job(int i, int d, int p) {
+            id = i;
+            deadline = d;
+            profit = p;
+        }
+    }
+
+    public static void main(String[] args) {
+        int jobsInfo[][] = {{4, 20}, {1, 10}, {1, 40}, {1, 30}};
+
+        ArrayList<Job> jobs = new ArrayList<>();
+
+        for (int i = 0; i < jobsInfo.length; i++) {
+            jobs.add(new Job(i, jobsInfo[i][0], jobsInfo[i][1]));
+        }
+
+        Collections.sort(jobs, (obj1, obj2) -> obj2.profit - obj1.profit);
+
+        ArrayList<Integer> seq = new ArrayList<>();
+        int time = 0;
+        for (int i = 0; i < jobs.size(); i++) {
+            Job curr = jobs.get(i);
+            if (curr.deadline > time) {
+                seq.add(curr.id);
+                time++;
+            }
+        }
+
+        System.out.println("Maximum Jobs = " + seq.size());
+        for (int i = 0; i < seq.size(); i++) {
+            System.out.print(seq.get(i) + " ");
+        }
+        System.out.println();
+    }
+}
