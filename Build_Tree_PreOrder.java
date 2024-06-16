389afdec4160696baef5973a54ba28c490c105b9
Ayush2649 <142195140+Ayush2649@users.noreply.github.com>
2024-06-16 09:01:00 +0530

    Update Build_Tree_PreOrder.java

--- Build_Tree_PreOrder.java
+++ Build_Tree_PreOrder.java
@@ -28,12 +28,22 @@ public class Build_Tree_PreOrder{
 
             return newNode;
         }
-    }
 
+    public static void preOrder(Node root){
+        if(root == null){
+            return;
+        }
+        System.out.println(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+    }
+    
     public static void main(String args[]){
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
-        System.out.println(root.data);
+        // System.out.println(root.data);
+        tree.preOrder(root);
     } 
 }
