7279335e9aa70e1f2cb68494689a273bcdeada45
Ayush2649 <142195140+Ayush2649@users.noreply.github.com>
2024-06-16 10:10:49 +0530

    Add files via upload

---
+++ Sum_Of_Nodes.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Sum_Of_Nodes{
+    static class Node{
+        int data; 
+        Node left;
+        Node right;
+    
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+    }
+}
+
+    public static int sum(Node root){
+        if(root == null){
+            return 0;
+        }
+
+        int leftSum = sum(root.left);
+        int rightSum = sum(root.right);
+        return leftSum + rightSum + root.data;
+    }
+
+    public static void main(String args[]){
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.println(sum(root));
+    }
+}
