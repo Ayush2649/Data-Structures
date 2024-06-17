9de4d1df21e1ec5791754de2a32ba74de0acea22
Ayush2649 <142195140+Ayush2649@users.noreply.github.com>
2024-06-17 19:52:46 +0530

    Update Diameter_Of_A_Tree.java

--- Diameter_Of_A_Tree.java
+++ Diameter_Of_A_Tree.java
@@ -22,19 +22,43 @@ public class Diameter_Of_A_Tree{
         return Math.max(lh, rh) + 1;
     }
 
-    public static int Diameter(Node root){
+    public static int Diameter2(Node root){
         if(root == null){
             return 0;
         }
 
-        int leftDiam = Diameter(root.left);
+        int leftDiam = Diameter2(root.left);
         int leftht = height(root.left);
-        int rightDiam = Diameter(root.right);
+        int rightDiam = Diameter2(root.right);
         int rightht = height(root.right);
 
         int selfDiam = leftht + rightht + 1;
 
         return Math.max(selfDiam, Math.max(rightDiam, leftDiam));
+    }
+
+    static class Info{
+        int diam;
+        int ht;
+
+        public Info(int diam, int ht){
+            this.diam = diam;
+            this.ht = ht;
+        }
+    }
+
+    public static Info diameter(Node root){
+        if(root == null){
+            return new Info(0, 0);
+        }
+
+        Info leftInfo = diameter(root.left);
+        Info rightInfo = diameter(root.right);
+
+        int diam = Math.max(Math.max(leftInfo.diam, rightInfo.diam), leftInfo.ht + rightInfo.ht + 1);
+        int ht = Math.max(leftInfo.ht, rightInfo.ht) + 1;
+
+        return new Info(diam, ht);
     }
 
     public static void main(String args[]){
@@ -46,6 +70,7 @@ public class Diameter_Of_A_Tree{
         root.right.left = new Node(6);
         root.right.right = new Node(7);
 
-        System.out.println(Diameter(root));
+        // System.out.println(Diameter2(root));
+        System.out.println(diameter(root).diam);
     }
 }
