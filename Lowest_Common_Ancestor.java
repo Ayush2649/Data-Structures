c6959580664b6c55193de1996d84b04e0fc61c20
Ayush2649 <142195140+Ayush2649@users.noreply.github.com>
2024-06-19 17:50:36 +0530

    Update Lowest_Common_Ancestor.java

--- Lowest_Common_Ancestor.java
+++ Lowest_Common_Ancestor.java
@@ -52,6 +52,24 @@ public class Lowest_Common_Ancestor{
         return lca;
     }
 
+     public static Node lca2(Node root, int n1, int n2){
+        if(root == null || root.data == n1 || root.data == n2){
+            return root;
+        }
+
+        Node leftLca = lca2(root.left, n1, n2);
+        Node rigthLca = lca2(root.right, n1, n2);
+
+        if(rigthLca == null){
+            return leftLca;
+        }
+        if(leftLca == null){
+            return rigthLca;
+        }
+
+        return root;
+    }
+
     public static void main(String args[]){
         Node root = new Node(1);
         root.left = new Node(2);
@@ -63,7 +81,8 @@ public class Lowest_Common_Ancestor{
 
         int n1 = 4; 
         int n2 = 5;
-        System.out.println(lca(root, n1, n2).data);
+        // System.out.println(lca(root, n1, n2).data);
+        System.out.println(lca2(root, n1, n2).data);
 
     }
 }
