04b9856633855c40575fef5052ff04e9da4d83b0
Ayush2649 <142195140+Ayush2649@users.noreply.github.com>
2024-06-22 08:02:52 +0530

    Update and rename Delete_A_Node_BST.java to BST_Operations.java

--- BST_Operations.java
+++ BST_Operations.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Delete_A_Node_BST{
+public class BST_Operations{
 
     static class Node{
         int data;
@@ -87,6 +87,27 @@ public class Delete_A_Node_BST{
         }
     }
 
+    public static void printPath(ArrayList<Integer> path){
+        for(int i = 0; i < path.size(); i++){
+            System.out.print(path.get(i) + "->");
+        }
+        System.out.println("Null");
+    }
+
+    public static void printRoot2Leaf(Node root, ArrayList<Integer> path){
+        if(root == null){
+            return;
+        }
+
+        path.add(root.data);
+        if(root.left == null && root.right == null){
+            printPath(path);
+        }
+        printRoot2Leaf(root.left, path);
+        printRoot2Leaf(root.right, path);
+        path.remove(path.size() - 1);
+    }
+
     public static void main(String args[]){
         int values[] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
         Node root = null;
@@ -103,6 +124,8 @@ public class Delete_A_Node_BST{
 
         // inOrder(root);
 
-        printInRange(root, 5, 12);
+        // printInRange(root, 5, 12);
+
+        printRoot2Leaf(root, new ArrayList<>());
     }
 }
