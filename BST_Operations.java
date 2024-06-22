8683f6884716e3fdd18cfef9f14987245a14077d
Ayush2649 <142195140+Ayush2649@users.noreply.github.com>
2024-06-22 08:38:38 +0530

    Update BST_Operations.java

--- BST_Operations.java
+++ BST_Operations.java
@@ -108,6 +108,21 @@ public class BST_Operations{
         path.remove(path.size() - 1);
     }
 
+    public static boolean isValid(Node root, Node min, Node max){
+        if(root == null){
+            return true;
+        }
+        if(min != null && root.data <= min.data){
+            return false;
+        }
+
+        else if(max != null && root.data >= max.data){
+            return false;
+        }
+
+        return isValid(root.left, min, max) && isValid(root.right, min, max);
+    }
+
     public static void main(String args[]){
         int values[] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
         Node root = null;
@@ -126,6 +141,12 @@ public class BST_Operations{
 
         // printInRange(root, 5, 12);
 
-        printRoot2Leaf(root, new ArrayList<>());
+        // printRoot2Leaf(root, new ArrayList<>());
+
+        if(isValid(root, null, null)){
+            System.out.println("Valid BST");
+        } else {
+            System.out.println("Not a valid BST");
+        }
     }
 }
