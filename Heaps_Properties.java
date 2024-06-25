3e9efe90b8350e7b2de53d715672ae0c1035362c
Ayush2649 <142195140+Ayush2649@users.noreply.github.com>
2024-06-25 21:12:07 +0530

    Update Heaps_Properties.java

--- Heaps_Properties.java
+++ Heaps_Properties.java
@@ -21,6 +21,28 @@ public class Heaps_Properties{
         public int peek(){
             return arr.get(0);
         }
+
+        public void heapify(int i){
+            int left = 2*i + 1;
+            int right = 2*i + 2;
+            int minIdx = i;
+
+            if(left < arr.size() && arr.get(minIdx) < arr.get(left)){  // For minHeap change to less than
+                minIdx = left;
+            }
+
+            if(right < arr.size() && arr.get(minIdx) < arr.get(right)){  // For minHeap change to less than
+                minIdx = right;
+            }
+
+            if(minIdx != i){
+                int temp = arr.get(i);
+                arr.set(i, arr.get(minIdx));
+                arr.set(minIdx, temp);
+
+                heapify(minIdx);
+            }
+        }
     }
 
     public static void main(String args[]){
