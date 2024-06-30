8219fa54afdbf0eefb34aec32a82aa4f020afe0f
Ayush2649 <142195140+Ayush2649@users.noreply.github.com>
2024-06-30 21:17:34 +0530

    Update Cycle_Detection_Directed_Graph.java

--- Cycle_Detection_Directed_Graph.java
+++ Cycle_Detection_Directed_Graph.java
@@ -22,6 +22,37 @@ public class Cycle_Detection_Directed_Graph{
         graph[3].add(new Edge(3, 0));
     }
 
+    public static boolean isCycle(ArrayList<Edge>[] graph){
+        boolean vis[] = new boolean[graph.length];
+        boolean stack[] = new boolean[graph.length];
+
+        for(int i = 0; i < graph.length; i++){
+            if(!vis[i]){
+                if(isCycleUtil(graph, i, vis, stack)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean isCycleUtil(ArrayList<Edge>[] graph, int curr, boolean vis[], boolean stack[]){
+        vis[curr] = true;
+        stack[curr] = true;
+
+        for(int i = 0; i < graph[curr].size(); i++){
+            Edge e = graph[curr].get(i);
+            if(stack[e.dest]){
+                return true;
+            }
+            if(!vis[e.dest] && isCycleUtil(graph, e.dest, vis, stack)){
+                return true;
+            }
+        }
+        stack[curr] = false;
+        return false;
+    }
+    
     public static void main(String args[]){
         int V = 4;
         ArrayList<Edge>[] graph = new ArrayList[V];
