de8f650a343bf189ebe82c1119a76dec5d39d75c
Ayush2649 <142195140+Ayush2649@users.noreply.github.com>
2024-06-30 20:58:15 +0530

    Add files via upload

---
+++ Cycle_Detection_Directed_Graph.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Cycle_Detection_Directed_Graph{
+
+    static class Edge{
+        int src;
+        int dest;
+
+        public Edge(int s, int d){
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    static void createGraph(ArrayList<Edge>[] graph){
+        for(int i = 0; i < graph.length; i++){
+            graph[i] = new ArrayList<>();
+        }
+        graph[0].add(new Edge(0, 2));
+        graph[1].add(new Edge(1, 0));
+        graph[2].add(new Edge(2, 3));
+        graph[3].add(new Edge(3, 0));
+    }
+
+    public static void main(String args[]){
+        int V = 4;
+        ArrayList<Edge>[] graph = new ArrayList[V];
+        createGraph(graph);
+    }
+}
