ba2cfe4eb80e6f4372600cf6ec63c9d9c1332265
Ayush2649 <142195140+Ayush2649@users.noreply.github.com>
2024-07-14 10:52:22 +0530

    Add files via upload

---
+++ Climbing_Stairs_TAbulation.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Climbing_Stairs_TAbulation{
+
+    public static int countWays(int n){ // O(n)
+        int dp[] = new int[n + 1];
+        dp[0] = 1;
+
+        for(int i = 1; i <= n; i++){
+            if(i == 1){
+                dp[i] = dp[i - 1] + 0;
+            } else {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+        }
+        return dp[n];
+    }
+
+    public static void main(String args[]){
+        int n = 5;
+        System.out.println(countWays(n));
+    }
+}
