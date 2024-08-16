05fc9eb7511ad6c0e01c7f57813fea1a5b8df8d6
Ayush2649 <142195140+Ayush2649@users.noreply.github.com>
2024-08-16 08:53:48 +0530

    Add files via upload

---
+++ H_Index.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class H_Index{
+
+    public static int hIndex(int[] citations){
+        int n = citations.length;
+        int[] arr = new int[n + 1];
+
+        for(int c : citations){
+            if(c >= n){
+                arr[n]++;
+            } else {
+                arr[c]++;
+            }
+        }
+
+        int count = 0;
+        for(int i = n; i >= 0; i--){
+            count = count + arr[i];
+            if(count >= i){
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args){
+        int[] citations = {3, 0, 6, 1, 5};
+        System.out.println(hIndex(citations));
+    }
+}
